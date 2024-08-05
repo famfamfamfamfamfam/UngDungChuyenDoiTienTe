9f57bf5fbf47f7a720c44d8f4b4ccc82b4a73d1a
BinhTran <fam.out16@gmail.com>
2024-08-06 05:24:38 +0700

    Add cac files cua bai tap hien thi loi chao

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HIENTHILOICHAO
+{
+    class LoiChaoTheoTen
+    {
+        static void Main(string[] args)
+        {
+            string tennguoidung;
+            Console.WriteLine("Nhap ten cua ban:");
+            tennguoidung = Console.ReadLine();
+            Console.WriteLine("Xin chao, {0}!", tennguoidung);
+        }
+    }
+}
